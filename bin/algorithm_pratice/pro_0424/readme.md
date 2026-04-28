a23284abb4ca6c70df9f4689586d1c635977503f
gkrud <rlagkrud90@ttmsoft.co.kr>
2026-04-28 12:33:47 +0900

    Brute_Force_피로도 ;

--- bin/algorithm_pratice/pro_0424/readme.md
+++ bin/algorithm_pratice/pro_0424/readme.md
@@ -1,2 +1,8 @@
+# Brute_Force_모의고사
 https://school.programmers.co.kr/learn/courses/30/lessons/42840
 <img width="1348" height="972" alt="image" src="https://github.com/user-attachments/assets/58488803-311b-4565-9766-624cf5965a52" />
+
+
+# Brute_Force_소수찾기
+https://school.programmers.co.kr/learn/courses/30/lessons/42839
+<img width="1383" height="828" alt="image" src="https://github.com/user-attachments/assets/d048740d-4b79-4fd9-8b7e-894a20e1fca3" />
---
+++ src/algorithm_pratice/pro_0428/Brute_Force_피로도.java
@@ -0,0 +1,40 @@
+package algorithm_pratice.pro_0428;
+ 
+public class Brute_Force_피로도 {
+
+	public static void main(String[] args) {
+		int[][] dungeons = {{80,20},{50,40},{30,10}};
+		int k = 80; 
+		
+		Solution sol = new Solution();
+		System.out.println(sol.solution(k, dungeons));
+	}
+
+}
+
+
+class Solution {
+
+    int maxDungeon = 0;
+    boolean[] visited; //던전 방문 여부
+    public int solution(int k, int[][] dungeons) {
+    	visited = new boolean[dungeons.length];
+    	dfs(k, 0, dungeons);
+        return maxDungeon;
+    }
+    
+    public void dfs(int k, int count, int[][] dungeons) {
+    	maxDungeon = Math.max(maxDungeon, count); //지금 까지 온 던전수가 count > maxDungeon 이면 
+    	
+    	for(int i=0;i<dungeons.length; i++) {
+    		if(!visited[i] && k >= dungeons[i][0]) {
+    			visited[i] = true;
+    			dfs(k-dungeons[i][1], count+1, dungeons);
+    			
+    			visited[i] = false;
+    		}
+    	}
+    }
+    
+}
+
